12a78c0e22a5b87ac0ddd4b55fc1e4483800642c
VICTORIA TRINOSOVA <trinosova.victoria@yandex.ru>
2021-11-27 15:30:01 +0300

    Добавьте файлы проекта.

---
+++ Lab 11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Решение уравнения kx+b=0");
+            Equation equation;
+            equation.k = 5;
+            equation.b = 10;
+            equation.Root();
+
+        }
+        struct Equation
+        {
+            public double k;
+            public double b;
+            public void Root()
+            {
+                double x;
+                x = b / k;
+                Console.WriteLine("x={0}", b / k);
+            }
+
+        }
+    }
+}
